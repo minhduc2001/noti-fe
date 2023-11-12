3a69ee2a5ad4e9c83a9d3fd218e4b9cc5a7def68
ducnm <ngoduc2468@gmail.com>
2023-11-12 23:21:22 +0700

    change

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,68 +1,95 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { getMessagingToken } from '../firebase';
+import React, { useState } from "react";
+import axios from "axios";
+import { getMessagingToken } from "../firebase";
 function Login() {
-    const host = 'http://127.0.0.1:3333'
-    const [value, setValue] = useState({});
-    const [token, setToken] = useState('');
+  const host = "http://127.0.0.1:3333";
+  const [value, setValue] = useState({});
+  const [token, setToken] = useState("");
 
-    const hanldeInput = (e)=>{
-        setValue({...value, [e.target.name]: e.target.value})
-    }
+  const hanldeInput = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        let data = value;
-        data.token = localStorage.getItem('token')
-        axios.post(host + '/user/login', data).then(res => {
-            console.log(res);
-        }).catch(e => {
-            console.log(e);
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let data = value;
+    data.token = localStorage.getItem("token");
+    axios
+      .post(host + "/user/login", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    const handleNotify = () =>{
-        axios.post(host + '/user/login', {}).then(res => {
-            console.log(res);
-        }).catch(e => {
-            console.log(e);
-        })
-    }
+  const handleSubmit1 = (e) => {
+    e.preventDefault();
+    let data = value;
+    data.token = localStorage.getItem("token");
+    axios
+      .post(host + "/user/register", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    const handleGroup = () => {
-        axios.post(host + '/user/send-group-noti', {}).then(res => {
-            console.log(res);
-        }).catch(e => {
-            console.log(e);
-        })
-    }
+  const handleNotify = () => {
+    axios
+      .post(host + "/user/login", {})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    const handleBoard = () =>{
-        axios.post(host + '/user/login', {}).then(res => {
-            console.log(res);
-        }).catch(e => {
-            console.log(e);
-        })
-    }
+  const handleGroup = () => {
+    axios
+      .post(host + "/user/send-group-noti", {})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    const generateToken = async () =>{
-        setToken(await getMessagingToken());
-    }
+  const handleBoard = () => {
+    axios
+      .post(host + "/user/login", {})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const generateToken = async () => {
+    setToken(await getMessagingToken());
+  };
 
   return (
     <div>
-        <input type="text" name='username' onChange={hanldeInput}/>
-        <input type="text" name='password' onChange={hanldeInput}/>
-        <button onClick={handleSubmit}>Submit</button>
+      <input type="text" name="username" onChange={hanldeInput} />
+      <input type="text" name="password" onChange={hanldeInput} />
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleSubmit1}>Submit RG</button>
 
-        <button onClick={handleNotify}>Get Notify</button>
-        <button onClick={handleGroup}>Send Group Notify</button>
-        <button onClick={handleBoard}>Send BoardCard Notify</button>
-        <button onClick={generateToken}>Generate Token</button>
+      <button onClick={handleNotify}>Get Notify</button>
+      <button onClick={handleGroup}>Send Group Notify</button>
+      <button onClick={handleBoard}>Send BoardCard Notify</button>
+      <button onClick={generateToken}>Generate Token</button>
 
-        <p>{token}</p>
+      <p>{token}</p>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
